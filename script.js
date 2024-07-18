78a456935327b3914f766dabb3428dad141767b8
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-07-18 15:50:13 +0300

    example added

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 let dizi = [10, 20, 30, 40];
+let names = ["nurdoğan", "okan", "emine", "simge", "alper"];
 
-function dizim(a, b, c, d) {
-  let toplam = a + b + c + d;
-  return toplam;
-}
+// function dizim(a, b, c, d) {
+//   let toplam = a + b + c + d;
+//   return toplam;
+// }
 
-console.log(dizim(...dizi));
+// console.log(dizim(...dizi));
+
+// const dizim = (a, b, c, d) => a - b - c - d;
+// console.log(dizim(...dizi));
+
+// const carpma = (a, b, c, d) => a * b * c * d;
+// console.log(carpma(...dizi));
+
+// const upperName = (a, b, c, d) => a + b + c + d;
+// console.log(upperName(...names));
+
+// names.map((name) => console.log(name.toUpperCase()));
+
+// let dizim = [...names]; discountşing yazım şekli
+// console.log(dizim);
+
+// dizim.forEach((x) => console.log(x.toUpperCase()));
